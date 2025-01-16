62b60e541756265105fedc5a72de738ad050fdf8
Gad Nadjar <gadnadjar@gmail.com>
2025-01-16 09:49:19 +0200

    fix bug overflow

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Hero from './components/Hero';
 import WhyChoose from './components/WhyChoose';
+import About from './components/About';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='overflow-x-hidden'>
+      <div className='overflow-x-hidden overflow-y-hidden'>
         <NavBar />
         <Hero />
         <Banner />
         <WhyChoose />
+        <About />
       </div>
     </>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+
+
+type Props = {}
+
+const About = (props: Props) => {
+    return (
+        <>
+            <div>
+                <div className="container">
+                    <h1 className="pt-20 tracking-wider text-4xl font-semiboldtext-white text-center">
+                        About Us
+                    </h1>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default About;
