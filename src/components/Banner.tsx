011f284ac7ae25fbf988779622f2461310b669ed
Gad Nadjar <gadnadjar@gmail.com>
2025-01-16 11:08:11 +0200

    add banner animation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,13 +9,22 @@ const Banner = () => {
     return (
         <>
             <div className='container py-14 relative'>
-                <div>
-                    <h1 className='py-8 tracking-wider text-2xl font-semibold text-dark text-center'>
+                <div className='relative z-20'>
+                    <h1
+                        data-aos='fade-up'
+                        data-aos-delay='300'
+                        className='py-8 tracking-wider text-2xl font-semibold text-dark text-center'>
                         Taste the Healthy Difference
                     </h1>
 
-                    <div className='space-y-10'>
-                        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 py-10'>
+                    <div
+                        data-aos='fade-up'
+                        data-aos-delay='500'
+                        className='space-y-10'>
+                        <div
+                            data-aos='fade-up'
+                            data-aos-delay='500'
+                            className='grid grid-cols-1 sm:grid-cols-2 gap-4 py-10'>
                             <div>
                                 <p className='text-justify'>
                                     We know that <span className='text-primary'>time</span> is the greatest value in the mordern world.
@@ -40,30 +49,46 @@ const Banner = () => {
                         </div>
                     </div>
 
-                    <div className='flex justify-center mt-10 sm:mt-14'>
+                    <div
+                        data-aos='fade-up'
+                        data-aos-delay='500'
+                        className='flex justify-center mt-10 sm:mt-14'>
                         <PrimaryButton />
                     </div>
                 </div>
 
                 {/* bg fruits pngs */}
-                <div className='absolute top-5 -left-16 sm:bottom-0 sm:left-0 opacity-50 sm:opacity-100'>
-                    <img src={leaf} className='max-w-[160px]' />
+                <div
+                    data-aos='fade-right'
+                    className='absolute top-5 -left-16 sm:bottom-0 sm:left-0 opacity-50 sm:opacity-100'>
+                    <img
+                        data-aos='fade-right'
+                        src={leaf} className='max-w-[160px]' />
                 </div>
 
-                <div className='absolute -bottom-16 -left-16 sm:bottom-0 sm:left-0 opacity-50 sm:opacity-100'>
-                    <img src={tomato} className='max-w-[280px]' />
+                <div
+                    data-aos='fade-right'
+                    className='absolute -bottom-16 -left-16 sm:bottom-0 sm:left-0 opacity-50 sm:opacity-100'>
+                    <img
+                        src={tomato} className='max-w-[280px]' />
                 </div>
 
-                <div className='absolute top-10 -right-16 sm:right-20 opacity-50 sm:opacity-100'>
+                <div
+                    data-aos='fade-left'
+                    className='absolute top-10 -right-16 sm:right-20 opacity-50 sm:opacity-100'>
                     <img src={lemon} className='max-w-[200px]' />
                 </div>
 
-                <div className='hidden sm:block absolute bottom-0 right-0'>
+                <div
+                    data-aos='fade-left'
+                    className='hidden sm:block absolute bottom-0 right-0'>
                     <img src={apple} className='max-w-[200px]' />
                 </div>
 
-                <div className='absolute top-1/2 -translate-y-1/2 left-1/3 -translate-x-1/2 opacity-50 sm:opacity-100'>
-                    <img src={kiwi} className='max-w-[180px]'/>
+                <div
+                    data-aos='fade'
+                    className='absolute top-1/2 -translate-y-1/2 left-1/3 -translate-x-1/2 opacity-50 sm:opacity-100'>
+                    <img src={kiwi} className='max-w-[180px]' />
                 </div>
             </div>
         </>
