650bd05d8bb9b979806111b82629cd742b5ee98f
Gad Nadjar <gadnadjar@gmail.com>
2025-01-16 09:46:36 +0200

    finish section

--- src/components/WhyChoose.tsx
+++ src/components/WhyChoose.tsx
@@ -16,7 +16,28 @@ const WhyChoose = () => {
                             <div className="text-center flex justify-center items-center flex-col gap-2 px-2">
                                 <p className='text-dark/70 font-semibold'>Lorem ipsum dolar sit amet ipsum dolar sit amet</p>
                                 <p className="text-5xl rotate-90 text-primary text-center translate-x-5">....</p>
-                                <FaBus className="text-5xl text-secondary " />
+                                <FaBus className="text-5xl text-primary translate-x-1" />
+                            </div>
+
+                            {/* second card */}
+                            <div className="text-center flex justify-center items-center flex-col gap-2 px-2">
+                                <FaBus className="text-5xl text-secondary translate-x-1 " />
+                                <p className="text-5xl rotate-90 text-secondary text-center translate-x-5">....</p>
+                                <p className='text-dark/70 font-semibold'>Lorem ipsum dolar sit amet ipsum dolar sit amet</p>
+                            </div>
+
+                            {/* third card */}
+                            <div className="text-center flex justify-center items-center flex-col gap-2 px-2">
+                                <p className='text-dark/70 font-semibold'>Lorem ipsum dolar sit amet ipsum dolar sit amet</p>
+                                <p className="text-5xl rotate-90 text-secondary sm:text-primary text-center translate-x-5">....</p>
+                                <FaBus className="text-5xl text-secondary sm:text-primary translate-x-1 " />
+                            </div>
+
+                            {/* fourth card */}
+                            <div className="text-center flex justify-center items-center flex-col gap-2 px-2">
+                                <FaBus className="text-5xl text-primary sm:text-secondary translate-x-1 " />
+                                <p className="text-5xl rotate-90 text-primary sm:text-secondary text-center translate-x-5">....</p>
+                                <p className='text-dark/70 font-semibold'>Lorem ipsum dolar sit amet ipsum dolar sit amet</p>
                             </div>
                         </div>
                     </div>
