db0276d58195246416d94c5481f6a2cd36da2099
Gad Nadjar <gadnadjar@gmail.com>
2025-01-16 09:49:40 +0200

    add app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Hero from './components/Hero';
